35cd372b21372ed8ec9152e589873462e934c0f4
vic-omo6 <victoryakhimien3@gmail.com>
2024-07-09 14:14:27 +0100

    calculator-app

---
+++ script.js
@@ -0,0 +1,88 @@
+let counter = document.getElementById('counter');
+
+
+function zero() {
+    counter.value += '0';
+}
+
+function one() {
+    counter.value += '1';
+}
+
+function two() {
+    counter.value += '2';
+}
+
+function three() {
+    counter.value += '3';
+}
+
+function four() {
+    counter.value += '4';
+}
+
+function five() {
+    counter.value += '5';
+}
+
+function six() {
+    counter.value += '6';
+}
+
+function seven() {
+    counter.value += '7';
+}
+
+function eight() {
+    counter.value += '8';
+}
+
+function nine() {
+    counter.value += '9';
+}
+
+function multiply() {
+    counter.value += '*';
+}
+
+function minus() {
+    counter.value += '-';
+}
+ 
+function dot() {
+    counter.value += '.';
+}
+
+function ac() {
+    counter.value = '';
+}
+
+function raise() {
+    counter.value += '**';
+}
+
+function root() {
+    counter.value = math.sqrt(counter.value);
+}
+
+function equals() {
+    counter.value =eval(counter.value);
+}
+
+function clearDisplay() {
+    counter.value = '';
+}
+
+function divide() {
+    counter.value  += '/';
+}
+
+
+
+
+
+
+
+
+
+
